3be07826b8cc6a7b43798f9e95c9774d6218c59d
Humphrey Marube <younghum254@gmail.com>
2026-04-29 16:44:45 +0300

    Fix API routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,22 @@
 <?php
 
 use App\Http\Controllers\API\VoteController;
-
-Route::get('/test', function () {
-    return response()->json(['status' => 'API working']);
-});
+use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [VoteController::class, 'register']);
 Route::post('/login', [VoteController::class, 'login']);
 Route::post('/forgot-password', [VoteController::class, 'forgotPassword']);
 Route::post('/reset-password', [VoteController::class, 'resetPassword']);
 Route::get('/candidates', [VoteController::class, 'getCandidates']);
+Route::get('/sectors', [VoteController::class, 'getSectors']);
+Route::get('/results', [VoteController::class, 'getResults']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/vote', [VoteController::class, 'castVote']);
     Route::get('/user', [VoteController::class, 'getUser']);
+    Route::post('/vote', [VoteController::class, 'castVote']);
+    Route::get('/my-vote', [VoteController::class, 'getMyVote']);
+    Route::get('/verify-vote/{hash}', [VoteController::class, 'verifyVote']);
     Route::post('/logout', [VoteController::class, 'logout']);
+    Route::post('/enroll-fingerprint', [VoteController::class, 'enrollFingerprint']);
+    Route::get('/voter-fingerprint', [VoteController::class, 'getVoterFingerprint']);
 });
